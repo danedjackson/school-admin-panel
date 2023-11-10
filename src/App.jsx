7a821c73d07b06a446f350489fe6505ea20898e4
Dane Jackson <danedjackson@yahoo.com>
2023-11-10 18:21:22 -0500

    Adding initial logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import Topbar from './scenes/global/Topbar';
 import Sidebar from './scenes/global/Sidebar';
 import Dashboard from './scenes/dashboard';
@@ -12,37 +13,44 @@ import ScoreForm from './scenes/score-form';
 import Calendar from './scenes/calendar';
 import FAQ from './scenes/faq';
 import StudentScoreDetails from './scenes/scores/studentScoreDetails';
+import Login from './scenes/login';
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [authenticated, setAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  // Check if the user is authenticated
+  if (!authenticated) {
+    // If not authenticated, redirect to the login page
+    navigate('/login');
+  }
 
   return (
-    // Global access to our color context
-    <ColorModeContext.Provider value = {colorMode}>
-      {/* Granting Material UI access */}
-      <ThemeProvider theme = {theme}>
-        {/* Resets css to defaults*/}
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
-        <div className = "app">
-          <Sidebar />
-          <main className = "content">
-            <Topbar />
+        <div className="app">
+          {authenticated && <Sidebar />}
+          <main className="content">
+            {authenticated && <Topbar />}
             <Routes>
-              <Route path = "/" element = {<Dashboard />} />
-              <Route path = "/teachers" element = {<Teachers />} />
-              <Route path = "/contacts" element = {<Contacts />} />
-              <Route path = "/scores" element = {<Scores />} />
-              <Route path = "/form" element = {<Form />} />
-              <Route path = "/score-form" element = {<ScoreForm />} />
-              <Route path = "/student-info" element = {<StudentScoreDetails />} />
-              <Route path = "/calendar" element = {<Calendar />} />
-              <Route path = "/faq" element = {<FAQ />} />
+              <Route path="/" element={authenticated ? <Dashboard /> : <Login />} />
+              <Route path="/teachers" element={authenticated ? <Teachers /> : <Login />} />
+              <Route path="/contacts" element={authenticated ? <Contacts /> : <Login />} />
+              <Route path="/scores" element={authenticated ? <Scores /> : <Login />} />
+              <Route path="/form" element={authenticated ? <Form /> : <Login />} />
+              <Route path="/score-form" element={authenticated ? <ScoreForm /> : <Login />} />
+              <Route path="/student-info" element={authenticated ? <StudentScoreDetails /> : <Login />} />
+              <Route path="/calendar" element={authenticated ? <Calendar /> : <Login />} />
+              <Route path="/faq" element={authenticated ? <FAQ /> : <Login />} />
+              <Route path="/login" element={authenticated ? null : <Login />} />
             </Routes>
           </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
