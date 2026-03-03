a4c07b213f6ec37ce60645dd55f4ef5144141904
Dane Jackson <danedjackson@yahoo.com>
2026-03-02 19:15:10 -0500

    Updated assignment list

--- src/data/transforms/subject.jsx
+++ src/data/transforms/subject.jsx
@@ -11,6 +11,16 @@ export const getAssignmentDropdownData = () => {
         { label: "Classwork", id: "classwork" },
         { label: "Unit Test", id: "unittest" },
         { label: "Mid-Term Examination", id: "midtermexam" },
-        { label: "End of Year Examination", id: "endofyearexam" }
+        { label: "End of Year Examination", id: "endofyearexam" },
+        { label: "End of Term Examination", id: "endoftermexam" },
+        { label: "Project", id: "project" },
+        { label: "Quiz", id: "quiz" },
+        { label: "Performance Task", id: "performancetask" },
+        { label: "Mock Exam", id: "mockexam" },
+        { label: "Portfolio", id: "portfolio" },
+        { label: "Diagnostic Assessment", id: "diagnostic" },
+        { label: "IDRI", id: "idri" },
+        { label: "GOILP", id: "goilp" },
+        { label: "Standardized Exam", id: "standardexam" }
       ]
 }
